23f2064f84fea44308482fa9ed2953ccfe5cc0c9
Roman Chuprikov <paradise.develop@gmail.com>
2018-05-31 23:47:11 +0500

    prevent memory leak

---
+++ client/Lykke.Service.RateCalculator.Client/RateCalculatorAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+// ReSharper disable once CheckNamespace
+namespace Lykke.Service.RateCalculator.Client.AutorestClient
+{
+    internal partial class RateCalculatorAPI
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// Should be used to prevent memory leak in RetryPolicy
+        /// </summary>
+        public RateCalculatorAPI(Uri baseUri, HttpClient client) : base(client)
+        {
+            Initialize();
+
+            BaseUri = baseUri ?? throw new ArgumentNullException("baseUri");
+        }
+    }
+}
