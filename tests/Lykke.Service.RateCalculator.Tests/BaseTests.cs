e0758215b6d3ac5312b537aab8f62f198ab67402
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-11-28 12:34:57 +0300

    Fixed tests

--- tests/Lykke.Service.RateCalculator.Tests/BaseTests.cs
+++ tests/Lykke.Service.RateCalculator.Tests/BaseTests.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using Common.Log;
 using Lykke.Service.RateCalculator.Core.Services;
 using Lykke.Service.RateCalculator.Services;
 using Lykke.Service.RateCalculator.Tests.Modules;
@@ -18,6 +19,8 @@ namespace Lykke.Service.RateCalculator.Tests
         private void RegisterDependencies()
         {
             var builder = new ContainerBuilder();
+
+            builder.RegisterInstance(new EmptyLog()).As<ILog>().SingleInstance();
 
             builder.RegisterModule(new RepositoriesModule());
 
